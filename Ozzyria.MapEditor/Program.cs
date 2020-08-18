4b4475b42b4d4830c7785e5ca07a021a97078787
Cole Garien <cgarien1@yahoo.com>
2020-08-18 08:33:14 -0500

    start adding in classes for tooling

--- Ozzyria.MapEditor/Program.cs
+++ Ozzyria.MapEditor/Program.cs
@@ -9,6 +9,72 @@ namespace Ozzyria.MapEditor
 {
     class Program
     {
+        enum ToolType
+        {
+            Pan,
+            Paint,
+            Erase
+        }
+
+        enum BrushType
+        {
+            Ground,
+            Water,
+            Fence
+        }
+
+
+        class MapViewerSettings
+        {
+            public int TileMapWidth { get; set; } = 24;
+            public int TileMapHeight { get; set; } = 24;
+
+            public float MapOffsetX { get; set; } = 0f;
+            public float MapOffsetY { get; set; } = 0f;
+            public float Zoom { get; set; } = 1f;
+
+            public float TileSize { get => 32f * Zoom; }
+
+            public float TileToScreenX(int TileX)
+            {
+                return MapOffsetX + (TileX * TileSize);
+            }
+            public float TileToScreenY(int TileY)
+            {
+                return MapOffsetY + (TileY * TileSize);
+            }
+
+            public int ScreenToTileX(float ScreenX)
+            {
+                return (int)Math.Floor((ScreenX - MapOffsetX) / TileSize);
+            }
+            public int ScreenToTileY(float ScreenY)
+            {
+                return (int)Math.Floor((ScreenY - MapOffsetY) / TileSize);
+            }
+        }
+
+        class Tool
+        {
+            public float PreviousX { get; set; }
+            public float PreviousY { get; set; }
+            public float X { get; set; }
+            public float Y { get; set; }
+            public float DeltaX { get => X - PreviousX; }
+            public float DeltaY { get => Y - PreviousY; }
+
+            public ToolType Type { get; set; } = ToolType.Pan;
+
+            public void MoveTool(float x, float y)
+            {
+                PreviousX = X;
+                PreviousY = Y;
+                X = x;
+                Y = y;
+            }
+
+        }
+
         static void Main(string[] args)
         {
             RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Ozzyria");
@@ -18,24 +84,21 @@ namespace Ozzyria.MapEditor
             };
             var font = new Font("Fonts\\Bitter-Regular.otf");
 
-            var controlled = new Entity();
-            controlled.AttachComponent(new Movement() { X = 100, Y = 100, PreviousX = 100, PreviousY = 100 });
-            controlled.AttachComponent(new BoundingBox() { Width = 20, Height = 20 });
+            var settings = new MapViewerSettings
+            {
+                TileMapWidth = 32,
+                TileMapHeight = 32,
+                MapOffsetX = 0,
+                MapOffsetY = 0
+            };
 
-
-            var tileSize = 32f;
-            var mapWidth = 32;
-            var mapHeight = 32;
-            var mapXOffset = 0f;
-            var mapYOffset = 0f;
-            var map = new int[mapWidth, mapHeight];
-
-            var mouseCurrentPosition = Mouse.GetPosition(window);
-            var mousePreviousPosition = mouseCurrentPosition;
+            var tool = new Tool();
+            var mousePosition = Mouse.GetPosition(window);
+            tool.MoveTool(mousePosition.X, mousePosition.Y);
 
             var contextMenuOpen = false;
-            var contextTileX = -1;
-            var contextTileY = -1;
+            var contextTileX = 0f;
+            var contextTileY = 0f;
 
             Stopwatch stopWatch = new Stopwatch();
             var deltaTime = 0f;
@@ -61,36 +124,34 @@ namespace Ozzyria.MapEditor
                 };
 
 
-                mousePreviousPosition = mouseCurrentPosition;
-                mouseCurrentPosition = Mouse.GetPosition(window);
+                mousePosition = Mouse.GetPosition(window);
+                tool.MoveTool(mousePosition.X, mousePosition.Y);
                 if (Mouse.IsButtonPressed(Mouse.Button.Left))
                 {
                     contextMenuOpen = false;
-                    contextTileX = -1;
-                    contextTileY = -1;
 
-                    mapXOffset += mouseCurrentPosition.X - mousePreviousPosition.X;
-                    mapYOffset += mouseCurrentPosition.Y - mousePreviousPosition.Y;
+                    settings.MapOffsetX += tool.DeltaX;
+                    settings.MapOffsetY += tool.DeltaY;
                 }
                 else if (Mouse.IsButtonPressed(Mouse.Button.Right))
                 {
                     contextMenuOpen = true;
-                    contextTileX = mouseCurrentPosition.X;
-                    contextTileY = mouseCurrentPosition.Y;
+                    contextTileX = tool.X;
+                    contextTileY = tool.Y;
                 }
 
                 // DRAW STUFF
                 window.Clear();
 
-                for(var x = 0; x < mapWidth; x++)
+                for(var x = 0; x < settings.TileMapWidth; x++)
                 {
-                    for (var y = 0; y < mapHeight; y++)
+                    for (var y = 0; y < settings.TileMapHeight; y++)
                     {
-                        var shape = new RectangleShape(new SFML.System.Vector2f(tileSize, tileSize));
+                        var shape = new RectangleShape(new SFML.System.Vector2f(settings.TileSize, settings.TileSize));
                         shape.FillColor = Color.Transparent;
                         shape.OutlineColor = Color.White;
                         shape.OutlineThickness = 1;
-                        shape.Position = new SFML.System.Vector2f(mapXOffset + (x * tileSize), mapYOffset + (y * tileSize));
+                        shape.Position = new SFML.System.Vector2f(settings.TileToScreenX(x), settings.TileToScreenY(y));
 
                         window.Draw(shape);
                     }
