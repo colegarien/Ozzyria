889f9efd5722fb8942616d29bd4fa9f75ceaede2
Cole Garien <cgarien1@yahoo.com>
2020-09-13 10:48:20 -0500

    start re-working map editor

--- Ozzyria.MapEditor/Program.cs
+++ Ozzyria.MapEditor/Program.cs
@@ -56,36 +56,11 @@ namespace Ozzyria.MapEditor
             }
         }
 
-        class Tool
-        {
-            public float PreviousX { get; set; }
-            public float PreviousY { get; set; }
-            public float X { get; set; }
-            public float Y { get; set; }
-            public float DeltaX { get => X - PreviousX; }
-            public float DeltaY { get => Y - PreviousY; }
-            public int Layer { get; set; } = 0;
-
-            public ToolType Type { get; set; } = ToolType.Pan;
-            public BrushType Brush { get; set; } = BrushType.Ground;
-
-            public void MoveTool(float x, float y)
-            {
-                PreviousX = X;
-                PreviousY = Y;
-                X = x;
-                Y = y;
-            }
-
-        }
-
         static void Main(string[] args)
         {
             RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Ozzyria");
-            window.Closed += (sender, e) =>
-            {
-                ((Window)sender).Close();
-            };
+            ViewWindow viewWindow = new ViewWindow((int)window.Size.X, (int)window.Size.Y);
+
             var font = new Font("Fonts\\Bitter-Regular.otf");
             var keyDelayTime = 200f;
             var keyTimer = keyDelayTime;
@@ -98,34 +73,53 @@ namespace Ozzyria.MapEditor
                 MapOffsetY = 0
             };
 
-            var tiles = new Dictionary<int, int[,]>();
-            for (var layer = 0; layer < settings.MaxLayers; layer++)
+            window.Closed += (sender, e) =>
             {
-                tiles[layer] = new int[settings.TileMapWidth, settings.TileMapHeight];
-            }
+                ((Window)sender).Close();
+            };
 
-            var tool = new Tool();
-            var mousePosition = Mouse.GetPosition(window);
-            tool.MoveTool(mousePosition.X, mousePosition.Y);
-
+            var ctrlKeyDown = false;
+            var altKeyDown = false;
+            window.KeyPressed += (sender, e) =>
+            {
+                if (e.Code == Keyboard.Key.LControl || e.Code == Keyboard.Key.RControl)
+                {
+                    ctrlKeyDown = true;
+                }
+                else if (e.Code == Keyboard.Key.LAlt || e.Code == Keyboard.Key.RAlt)
+                {
+                    altKeyDown = true;
+                }
+            };
+            window.KeyReleased += (sender, e) =>
+            {
+                if (e.Code == Keyboard.Key.LControl || e.Code == Keyboard.Key.RControl)
+                {
+                    ctrlKeyDown = false;
+                }
+                else if (e.Code == Keyboard.Key.LAlt || e.Code == Keyboard.Key.RAlt)
+                {
+                    altKeyDown = false;
+                }
+            };
             window.MouseWheelScrolled += (sender, e) =>
             {
-                if (e.Delta == 1)
+                if(e.Wheel == Mouse.Wheel.HorizontalWheel || (altKeyDown && e.Wheel == Mouse.Wheel.VerticalWheel))
                 {
-                    // Scroll through tools
-                    tool.Type = (ToolType)(((int)tool.Type + 1) % Enum.GetValues(typeof(ToolType)).Length);
+                    viewWindow.OnHorizontalScroll(e.Delta);
                 }
-                if (e.Delta == -1)
+                else if (ctrlKeyDown)
                 {
-                    // Scroll through brushes
-                    tool.Brush = (BrushType)(((int)tool.Brush+1) % Enum.GetValues(typeof(BrushType)).Length);
-                    
+                    viewWindow.OnZoom(e.X, e.Y, e.Delta);
+                }
+                else {
+                    viewWindow.OnVerticalScroll(e.Delta);
                 }
             };
 
-            var contextMenuOpen = false;
-            var contextTileX = 0f;
-            var contextTileY = 0f;
+
+
+            var mousePosition = Mouse.GetPosition(window);
 
             Stopwatch stopWatch = new Stopwatch();
             var deltaTime = 0f;
@@ -139,16 +133,6 @@ namespace Ozzyria.MapEditor
                 ///
                 window.DispatchEvents();
                 var quit = window.HasFocus() && Keyboard.IsKeyPressed(Keyboard.Key.Escape);
-                var input = new Input
-                {
-                    MoveUp = window.HasFocus() && Keyboard.IsKeyPressed(Keyboard.Key.W),
-                    MoveDown = window.HasFocus() && Keyboard.IsKeyPressed(Keyboard.Key.S),
-                    MoveLeft = window.HasFocus() && Keyboard.IsKeyPressed(Keyboard.Key.A),
-                    MoveRight = window.HasFocus() && Keyboard.IsKeyPressed(Keyboard.Key.D),
-                    TurnLeft = window.HasFocus() && Keyboard.IsKeyPressed(Keyboard.Key.Q),
-                    TurnRight = window.HasFocus() && Keyboard.IsKeyPressed(Keyboard.Key.E),
-                    Attack = window.HasFocus() && Keyboard.IsKeyPressed(Keyboard.Key.Space)
-                };
 
                 if (keyTimer < keyDelayTime)
                 {
@@ -159,101 +143,84 @@ namespace Ozzyria.MapEditor
                 if(Mouse.IsButtonPressed(Mouse.Button.Middle) && keyTimer >= keyDelayTime)
                 {
                     keyTimer = 0f;
-                    tool.Layer = (tool.Layer + 1) % settings.MaxLayers;
                 }
 
                 mousePosition = Mouse.GetPosition(window);
-                tool.MoveTool(mousePosition.X, mousePosition.Y);
+
                 if (Mouse.IsButtonPressed(Mouse.Button.Left))
                 {
-                    contextMenuOpen = false;
-
-                    if (tool.Type == ToolType.Pan)
-                    {
-                        settings.MapOffsetX += tool.DeltaX;
-                        settings.MapOffsetY += tool.DeltaY;
-                    }
-                    else if(tool.Type == ToolType.Paint)
-                    {
-                        var tileX = settings.ScreenToTileX(tool.X);
-                        var tileY = settings.ScreenToTileY(tool.Y);
-
-                        if(tileX >= 0 && tileX < settings.TileMapWidth && tileY >= 0 && tileY < settings.TileMapHeight)
-                        {
-                            tiles[tool.Layer][tileX, tileY] = (int)tool.Brush;
-                        }
-                    }
+                    
                 }
                 else if (Mouse.IsButtonPressed(Mouse.Button.Right))
                 {
-                    contextMenuOpen = true;
-                    contextTileX = tool.X;
-                    contextTileY = tool.Y;
+
                 }
 
                 // DRAW STUFF
                 window.Clear();
 
-                for(var x = 0; x < settings.TileMapWidth; x++)
-                {
-                    for (var y = 0; y < settings.TileMapHeight; y++)
-                    {
-                        var shape = new RectangleShape(new SFML.System.Vector2f(settings.TileSize, settings.TileSize));
-                        switch(tiles[tool.Layer][x, y])
-                        {
-                            case (int)BrushType.Ground:
-                                shape.FillColor = Color.Green;
-                                break;
-                            case (int)BrushType.Water:
-                                shape.FillColor = Color.Blue;
-                                break;
-                            case (int)BrushType.Fence:
-                                shape.FillColor = Color.Red;
-                                break;
-                            default:
-                                shape.FillColor = Color.Transparent;
-                                break;
-                        }
-                        shape.OutlineColor = Color.White;
-                        shape.OutlineThickness = 1;
-                        shape.Position = new SFML.System.Vector2f(settings.TileToScreenX(x), settings.TileToScreenY(y));
+                //for (var x = 0; x < settings.TileMapWidth; x++)
+                //{
+                //    for (var y = 0; y < settings.TileMapHeight; y++)
+                //    {
+                //        var shape = new RectangleShape(new SFML.System.Vector2f(settings.TileSize, settings.TileSize));
+                //        switch (tiles[tool.Layer][x, y])
+                //        {
+                //            case (int)BrushType.Ground:
+                //                shape.FillColor = Color.Green;
+                //                break;
+                //            case (int)BrushType.Water:
+                //                shape.FillColor = Color.Blue;
+                //                break;
+                //            case (int)BrushType.Fence:
+                //                shape.FillColor = Color.Red;
+                //                break;
+                //            default:
+                //                shape.FillColor = Color.Transparent;
+                //                break;
+                //        }
+                //        shape.OutlineColor = Color.White;
+                //        shape.OutlineThickness = 1;
+                //        shape.Position = new SFML.System.Vector2f(settings.TileToScreenX(x), settings.TileToScreenY(y));
 
-                        window.Draw(shape);
-                    }
-                }
+                //        window.Draw(shape);
+                //    }
+                //}
+
+                viewWindow.OnRender(window);
 
                 var cursor = new RectangleShape(new SFML.System.Vector2f(settings.TileSize, settings.TileSize));
                 cursor.FillColor = Color.Transparent;
                 cursor.OutlineColor = Color.Blue;
                 cursor.OutlineThickness = 4;
-                cursor.Position = new SFML.System.Vector2f(settings.TileToScreenX(settings.ScreenToTileX(tool.X)), settings.TileToScreenY(settings.ScreenToTileY(tool.Y)));
+                // cursor.Position = new SFML.System.Vector2f(settings.TileToScreenX(settings.ScreenToTileX(tool.X)), settings.TileToScreenY(settings.ScreenToTileY(tool.Y)));
                 window.Draw(cursor);
 
-                if (contextMenuOpen)
-                {
-                    var shape = new RectangleShape(new SFML.System.Vector2f(100, 200));
-                    shape.FillColor = Color.Blue;
-                    shape.OutlineColor = Color.White;
-                    shape.OutlineThickness = 2;
-                    shape.Position = new SFML.System.Vector2f(contextTileX, contextTileY);
+                //if (contextMenuOpen)
+                //{
+                //    var shape = new RectangleShape(new SFML.System.Vector2f(100, 200));
+                //    shape.FillColor = Color.Blue;
+                //    shape.OutlineColor = Color.White;
+                //    shape.OutlineThickness = 2;
+                //    shape.Position = new SFML.System.Vector2f(contextTileX, contextTileY);
 
-                    window.Draw(shape);
+                //    window.Draw(shape);
 
 
-                    var someText = new Text();
-                    someText.CharacterSize = 16;
-                    someText.DisplayedString = "menu";
-                    someText.FillColor = Color.Red;
-                    someText.Font = font;
-                    someText.Position = new SFML.System.Vector2f(contextTileX, contextTileY);
+                //    var someText = new Text();
+                //    someText.CharacterSize = 16;
+                //    someText.DisplayedString = "menu";
+                //    someText.FillColor = Color.Red;
+                //    someText.Font = font;
+                //    someText.Position = new SFML.System.Vector2f(contextTileX, contextTileY);
 
-                    window.Draw(someText);
-                }
+                //    window.Draw(someText);
+                //}
 
                 // DEBUG STUFF
                 var debugText = new Text();
                 debugText.CharacterSize = 32;
-                debugText.DisplayedString = $"Layer: {tool.Layer}\nTool Type: {tool.Type}\nBrush Type: {tool.Brush}\nTile X: {settings.ScreenToTileX(tool.X)}\nTile Y: {settings.ScreenToTileY(tool.Y)}";
+                debugText.DisplayedString = $"Ctrl: {ctrlKeyDown}" ;
                 debugText.FillColor = Color.Red;
                 debugText.OutlineColor = Color.Black;
                 debugText.OutlineThickness = 1;
---
+++ Ozzyria.MapEditor/ViewWindow.cs
@@ -0,0 +1,115 @@
+﻿using SFML.Graphics;
+
+namespace Ozzyria.MapEditor
+{
+    class ViewWindow
+    {
+        private const float hScrollSensitivity = 5f;
+        private const float vScrollSensitivity = 5f;
+        private const float zoomSensitivity = 0.01f;
+
+        private int width;
+        private int height;
+        private float xOffset = 0f;
+        private float yOffset = 0f;
+        private float zoomPercent = 1f;
+
+        private Map _map;
+        private RenderTexture _renderBuffer;
+
+        public ViewWindow(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            _map = new Map
+            {
+                Width = 10,
+                Height = 10
+            }; // TODO have like a 'un/load map' and make _map nullable
+            _renderBuffer = new RenderTexture((uint)(_map.Width * _map.TileDimension), (uint)(_map.Width * _map.TileDimension));
+
+            // ReCenter on map
+            xOffset = 0; //((_map.Width * _map.TileDimension) * 0.5f) - (this.width * 0.5f);
+            yOffset = 0; //((_map.Height * _map.TileDimension) * 0.5f) - (this.height * 0.5f);
+            zoomPercent = 1f;
+        }
+
+        public void OnHorizontalScroll(float delta) {
+            xOffset += delta * hScrollSensitivity;
+        }
+
+        public void OnVerticalScroll(float delta)
+        {
+            yOffset -= delta * vScrollSensitivity;
+        }
+
+        public void OnZoom(int xOrigin, int yOrigin, float delta)
+        {
+            var previousZoomPercent = zoomPercent;
+            zoomPercent += delta * zoomSensitivity;
+            
+            if(zoomPercent < 0.01f)
+            {
+                zoomPercent = 0.01f;
+            }
+            else if(zoomPercent > 200f)
+            {
+                zoomPercent = 200f;
+            }
+
+            //var deltaZoom = zoomPercent - previousZoomPercent;
+            //var worldX =  (xOrigin * zoomPercent) - (xOrigin * previousZoomPercent);
+            //var worldY = (yOrigin * zoomPercent) - (yOrigin * previousZoomPercent);
+
+            //xOffset += xOrigin * deltaZoom;
+            //yOffset += yOrigin * deltaZoom;
+            //xOffset -= (((xOrigin + (xOffset)) * zoomPercent) - ((xOrigin + (xOffset)) * previousZoomPercent));
+            //yOffset -= (((yOrigin + (yOffset)) * zoomPercent) - ((yOrigin + (yOffset)) * previousZoomPercent));
+            // TODO zoom on xOrigin + yOrigin (if I remember right this is called 'Affine Transformation')
+        }
+
+        public void OnRender(RenderWindow window)
+        {
+            _renderBuffer.Clear();
+            if (_map == null)
+            {
+                return;
+            }
+
+            var tileDimension = _map.TileDimension;
+
+            for (var x = 0; x < _map.Width; x++)
+            {
+                for (var y = 0; y < _map.Height; y++)
+                {
+                    var tileShape = new RectangleShape(new SFML.System.Vector2f(tileDimension, tileDimension));
+                    tileShape.Position = new SFML.System.Vector2f((x * tileDimension), (y * tileDimension));
+
+                    _renderBuffer.Draw(tileShape);
+                }
+            }
+
+            for (var x = 0; x < _map.Width; x++)
+            {
+                for (var y = 0; y < _map.Height; y++)
+                {
+                    var overlayBorder = new RectangleShape(new SFML.System.Vector2f(tileDimension, tileDimension));
+                    overlayBorder.Position = new SFML.System.Vector2f((x * tileDimension), (y * tileDimension));
+                    overlayBorder.FillColor = Color.Transparent;
+                    overlayBorder.OutlineThickness = 2;
+                    overlayBorder.OutlineColor = Color.Black;
+
+                    _renderBuffer.Draw(overlayBorder);
+                }
+            }
+
+            _renderBuffer.Display();
+            window.Draw(new Sprite(_renderBuffer.Texture)
+            {
+                Position = new SFML.System.Vector2f(-xOffset, -yOffset),
+                Scale = new SFML.System.Vector2f(zoomPercent, zoomPercent)
+            });
+        }
+    }
+}
