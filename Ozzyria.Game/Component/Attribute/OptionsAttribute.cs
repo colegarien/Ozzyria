853ba4b5970d84bbf1d60a1544a6f3ab14bb2f60
Cole Garien <colegarien@gmail.com>
2021-02-07 21:05:32 -0600

    shift todos over to correct project

--- Ozzyria.Game/Component/Attribute/OptionsAttribute.cs
+++ Ozzyria.Game/Component/Attribute/OptionsAttribute.cs
@@ -3,7 +3,7 @@
 namespace Ozzyria.Game.Component.Attribute
 {
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-    public class OptionsAttribute : System.Attribute // TODO OZ-18 flush this out, store component type, name, etc
+    public class OptionsAttribute : System.Attribute // TODO OZ-14 flush this out, store component type, name, etc
     {
         public string Name { get; set; } = "";
     }
