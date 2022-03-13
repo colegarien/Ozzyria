e89f88e82df165cf63f872f3e465a1862554817c
Cole Garien <colegarien@gmail.com>
2022-03-13 14:49:33 -0500

    OZ-17 : remove old map editor

--- Ozzyria.MapEditor/EventSystem/MouseDownEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ozzyria.MapEditor.EventSystem
-{
-    class MouseDownEvent : WindowSpecificEvent
-    {
-        public bool LeftMouseDown { get; set; }
-        public bool RightMouseDown { get; set; }
-        public bool MiddleMouseDown { get; set; }
-    }
-}
